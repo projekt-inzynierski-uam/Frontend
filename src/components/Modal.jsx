8e1649092729751003990e0dfa129c4be8d0913a
KirkeG <jantanollio1233@gmail.com>
2023-06-29 23:08:11 +0200

    Components_And_Assets
    Pushing some components

---
+++ src/components/Modal.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react"
+import { useCookies } from 'react-cookie'
+import { DateInput } from '@mantine/dates';
+
+const Modal = ({mode, setShowModal, getData, task}) => {
+    const editMode = mode === "edit" ? true : false
+    const [cookies, setCookie, removeCookie] = useCookies(null)
+    const [date, setDate] = useState(null);
+
+    const [data, setData] = useState({
+      user_email: editMode ? task.user_email : cookies.Email,
+      title: editMode ? task.title : null,
+    })
+
+    const postData = async (e) => {
+      e.preventDefault()
+      try {
+        const response = await fetch(`http://localhost:8000/todos`, {
+          method: "POST",
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(data)
+        })
+
+        if(response.status === 200){
+          setShowModal(false)
+          getData()
+        }
+
+      }catch(err){
+        console.error(err)
+      }
+    }
+
+    const editData = async (e) => {
+      e.preventDefault()
+      try{
+        const response = await fetch(`http://localhost:8000/todos/${task.id}`,{
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json'},
+          body: JSON.stringify(data)
+      })
+      if(response.status === 200){
+          setShowModal(false)
+          getData()
+      }
+      }catch(err){
+        console.error(err)
+      }
+    }
+
+    const handleChange = (e) => {
+      const {name, value} = e.target
+
+      setData(data => ({
+        ...data,
+        [name] : value
+      }))
+    }
+
+    return (
+      <>
+        <div className="overlay">
+          <div className="modal">
+            <div className="form-title-container">
+              <h3>{mode} zadanie</h3>
+              <button onClick = {() => setShowModal(false)}>X</button>
+            </div>
+
+            <form>
+              <input
+                required
+                maxLength={30}
+                placeholder="Nazwa zadania"
+                name="title"
+                value={data.title}
+                onChange={handleChange}
+              />
+              <DateInput
+                onChange={setDate}
+                label="Wybierz datę"
+                placeholder="Data"
+                maw={150}
+                mx="marginRight"
+              />
+              <br/>
+              <input className={mode} type="submit" onClick={editMode ? editData : postData}/>
+            </form>
+          </div>
+        </div>
+      </>
+    )
+  }
+  
+  export default Modal
+  
