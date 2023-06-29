6e6f6b015063bf80552d596a3d8e520147bdced4
Michukpk <131003834+Michukpk@users.noreply.github.com>
2023-06-29 17:07:36 +0200

    homepage

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react'
+import ListItem from './components/ListItem'
+import ListHeader from './components/ListHeader'
+import Auth from './components/Auth'
+import {useCookies} from 'react-cookie'
+import './index.css'
+
+function App() {
+
+  const [cookies, setCookie, removeCookie] = useCookies(null)
+  const userEmail = cookies.Email
+  const authToken = cookies.AuthToken
+  const [tasks, setTasks] = useState(null)
+
+  const getData = async () => {
+    try{
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`)
+      const json = await response.json()
+      setTasks(json)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    if(authToken){
+      getData()
+    }
+  }, [authToken])
+
+  console.log(tasks)
+
+  return (
+    <>
+    
+      <div className="app">
+        {!authToken && <Auth/>}
+        {authToken &&
+        <>
+        <p className="user-email">Witaj {userEmail} </p>
+        <ListHeader listName={'Lista Zadań'} getData={getData}/>
+        {tasks?.map((task) => <ListItem key={task.id} task={task} getData={getData}/>)}
+        </>}
+      </div>
+    </>
+  )
+}
+
+export default App
