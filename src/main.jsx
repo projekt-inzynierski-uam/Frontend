1a731a1ea0d7b2b9547099612ec8688ac1f1a3f1
KirkeG <jantanollio1233@gmail.com>
2023-11-23 20:19:43 +0100

    feat: add ProtectedRoute.tsx and protect dashboard routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import {RouterProvider} from 'react-router-dom'
-import router from './router'
+import { router } from './routes/router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <RouterProvider router={router}/>
---
+++ src/routes/paths.ts
@@ -0,0 +1,10 @@
+export const Paths = {
+  HOME: '/',
+  LOGIN: '/login',
+  DASHBOARD: '/dashboard',
+  DASHBOARD_CALENDAR: '/dashboard/kalendarz',
+  DASHBOARD_TASKS: '/dashboard/zadania',
+  DASHBOARD_GROUPS: '/dashboard/grupy',
+  DASHBOARD_GROUP_PAGE: '/dashboard/grupy/:idGrupy',
+  DASHBOARD_SETTINGS: '/dashboard/ustawienia/'
+} as const
