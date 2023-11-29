796ae54e6eeb6480c212b5135c1a9aa710a50756
Mateusz Smolarz <msmolarztech@gmail.com>
2023-11-29 16:11:58 +0100

    update: mantine version 7

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
+import '@mantine/core/styles.css';
 
 import App from './App'
+import { MantineProvider, createTheme } from '@mantine/core';
+
+const theme = createTheme({
+  breakpoints: {
+    xs: '30em',
+    sm: '48em',
+    md: '64em',
+    lg: '74em',
+    xl: '90em',
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </StrictMode>,
 )
