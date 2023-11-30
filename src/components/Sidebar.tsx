c9a99fa66231b19982d1286dab1121b2813e5057
Mateusz Smolarz <80050774+crabyms@users.noreply.github.com>
2023-11-30 18:52:34 +0100

    Dashboard to appshell (#34)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,29 +1,50 @@
-import { Link } from 'react-router-dom'
-import '../styles/Sidebar.css'
-
 import { RedirectButton } from './RedirectButton'
-import { AppTitle } from './AppTitle'
 import { Paths } from '../routes/paths'
-import SettingsIcon from '../assets/settingsicon.png'
+import { Flex, Center, Divider, Title } from '@mantine/core'
+import PanelIcon from '../assets/panelicon.svg'
+import ZadaniaIcon from '../assets/menedzerzadan.svg'
+import CeleIcon from '../assets/cele.svg'
 
 export const Sidebar = () => {
   return (
     <>
-      <div className="sidebar">
-        <div className="sidebar-content">
-          <AppTitle />
-          <div className="menu-text">Panel główny</div>
-          <div className="bar"></div>
-          <RedirectButton path={Paths.DASHBOARD_TASKS} name={'Zadania'} />
-          <RedirectButton path={Paths.DASHBOARD_GROUPS} name={'Grupy'} />
-          <div className="bar"></div>
-        </div>
-        <Link to={'/dashboard/ustawienia'}>
-          <div className="settings-bar">
-            <img src={SettingsIcon} alt="icon for settings" />
-          </div>
-        </Link>
-      </div>
+      <Flex
+        justify="center"
+        align="center"
+        direction="column"
+        gap="xl"
+        h="70%"
+      >
+        <Flex
+          justify="center"
+          align="center"
+          direction="column"
+          w="100%"
+          gap="xs"
+        >
+          <Center>
+            <Title order={1} c="white" ff="Oswald">Zadania</Title>
+          </Center>
+          <RedirectButton path={Paths.DASHBOARD_TASKS} name={'Panel'} icon={PanelIcon}/>
+          <RedirectButton path={Paths.DASHBOARD_GROUPS} name={'Zarządzanie zadaniami'} icon={ZadaniaIcon}/>
+          <RedirectButton path={Paths.DASHBOARD_GROUPS} name={'Zarządzanie celami'} icon={CeleIcon}/>
+        </Flex>
+        <Divider size="lg"/>
+        <Flex
+          justify="center"
+          align="center"
+          direction="column"
+          w="100%"
+          gap="xs"
+        >
+          <Center>
+            <Title order={1} c="white" ff="Oswald">Grupy</Title>
+          </Center>
+          <RedirectButton path={Paths.DASHBOARD_TASKS} name={'Zadania'} icon={""}/>
+          <RedirectButton path={Paths.DASHBOARD_GROUPS} name={'Grupy'} icon={""}/>
+          <RedirectButton path={Paths.DASHBOARD_GROUPS} name={'Grupy'} icon={""}/>
+        </Flex>
+      </Flex>
     </>
   )
 }
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,21 +1,37 @@
-import '../styles/Dashboard.css'
-import '../styles/index.css'
-
 import { Sidebar } from '../components/Sidebar'
 import { Outlet } from 'react-router-dom'
-import { Header } from '../components/Header'
+import { HeaderContent } from '../components/HeaderContent'
+
+import { useDisclosure } from '@mantine/hooks';
+import { AppShell, Group, Burger, Avatar } from '@mantine/core';
 
 const Dashboard = () => {
+  const [opened, {toggle}] = useDisclosure();
+
   return (
-    <div className="dashboard">
-      <Sidebar />
-      <div className="header-main-wrapper">
-        <Header />
-        <div className="main-content">
-          <Outlet />
-        </div>
-      </div>
-    </div>
+    <AppShell
+      header={{ height: { base: 80, md: 90, lg: 100 }}}
+      navbar={{
+        width: { base: 150, md: 200, lg: 250, xl: 250 },
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+      withBorder="false"
+    >
+      <AppShell.Header bg="#E85A4F" p="md" style={{ "border-bottom-left-radius": "20px", "border-bottom": "5px solid white" }}>
+        <Group h="100%" px="md" justify='space-between' >
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <HeaderContent/>
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md" bg="#E98074" style={{ "border-radius": "20px" }}>
+        <Sidebar/>
+      </AppShell.Navbar>
+      <AppShell.Main bg="#FFF5F3">
+        <Outlet/>
+      </AppShell.Main>
+    </AppShell>
   )
 }
 
