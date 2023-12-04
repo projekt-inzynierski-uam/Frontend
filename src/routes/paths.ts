d0657c4b59618864bf74b8582c286d57de4a81d0
Mateusz Smolarz <80050774+crabyms@users.noreply.github.com>
2023-12-04 14:31:04 +0100

    Panel zadan (#43)

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -4,7 +4,7 @@ export const Paths = {
   REGISTER: '/register',
   DASHBOARD: '/dashboard',
   DASHBOARD_CALENDAR: '/dashboard/kalendarz',
-  DASHBOARD_TASKS: '/dashboard/zadania',
+  DASHBOARD_DTASKS: '/dashboard/panel',
   DASHBOARD_GROUPS: '/dashboard/grupy',
   DASHBOARD_GROUP_TASKS: '/dashboard/grupy/:id',
   DASHBOARD_SETTINGS: '/dashboard/ustawienia/',
