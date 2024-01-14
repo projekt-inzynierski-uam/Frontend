d8fc5b9cdf53db6bf4344a451e232bd55c17636f
Mateusz Smolarz <80050774+crabyms@users.noreply.github.com>
2024-01-14 23:48:51 +0100

    change (#68)

--- src/components/GroupSettings.jsx
+++ src/components/GroupSettings.jsx
@@ -1,5 +1,6 @@
-import { Flex, Button, Text } from '@mantine/core'
+import { Flex, Button, Text, Group, TextInput } from '@mantine/core'
 import { useLocation } from 'react-router-dom'
+import { useForm } from '@mantine/form';
 
 const GroupSettings = () => {
     const location = useLocation()
@@ -15,16 +16,54 @@ const GroupSettings = () => {
         }
     }
 
+    const editGroupName = async (data) => {
+        try {
+          const response = await fetch(`${import.meta.env.VITE_DBSERVER}/changegroupname/${groupId}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data),
+          })
+        } catch (err) {
+          console.error(err)
+        }
+    }
+
+    const form = useForm({
+        initialValues: {
+          groupName: '',
+        },
+
+        validate: {
+            groupName: (value) => (value.length > 25 || value.length < 1 ? 'Zła długość nazwy': null), 
+        },
+    })
+
     return(
         <>
         <Flex
             justify="center"
             align="center"
             mih={800}
+            direction="column"
+            gap='xl'
         >
-            <Button onClick={deleteGroup} w="25%" h={140} style={{borderRadius:"20px"}} bg="#8E8D8A">
+            <Text size='35px' c="#E98074">Zmień nazwę grupy</Text>
+            <Group>
+                <Text c="#E98074" size='xl'>Nazwa Grupy:</Text>
+                <form onSubmit={form.onSubmit((values) => editGroupName(values))}>
+                    <Group>
+                        <TextInput
+                            {...form.getInputProps('groupName')}
+                        />
+                        <Button type="submit" h={50} style={{background: "#E98074", borderRadius:"20px"}}>
+                            <Text size='xl'>Zmień</Text>
+                        </Button>
+                    </Group>
+                </form>
+            </Group>    
+            <Button onClick={deleteGroup} w="25%" h={50} style={{borderRadius:"20px"}} bg="#8E8D8A">
                 <Text size='xl'>Usuń grupę</Text>
-            </Button>    
+            </Button>
         </Flex>
         </>
     )
