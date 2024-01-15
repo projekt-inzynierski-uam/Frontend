dd3847950ef7b4e7e546dae475a179e0f9f899f2
Mateusz Smolarz <80050774+crabyms@users.noreply.github.com>
2024-01-15 08:11:23 +0100

    add (#74)

--- src/components/profil.jsx
+++ src/components/profil.jsx
@@ -1,8 +1,19 @@
+import {Flex, Text} from '@mantine/core'
+import Cookies from 'js-cookie'
+import { CookieName } from '../lib/constants/cookies'
 
+const userEmail = Cookies.get(CookieName.EMAIL)
 
 const Profil = () => {
     return(
         <>
+        <Flex
+            justify='center'
+            align='center'
+            mih="600px"
+        >
+            <Text size='35px' c="#E98074">Witaj {userEmail}</Text>
+        </Flex>
         </>
     )
 }
