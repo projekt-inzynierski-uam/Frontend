f600e3a3c3a0771298ee37f426db374a644e1c07
Mateusz Smolarz <msmolarztech@gmail.com>
2024-01-15 03:20:54 +0100

    fix

--- src/components/ActiveObjective.jsx
+++ src/components/ActiveObjective.jsx
@@ -34,11 +34,11 @@ const ActiveObjective = ({email}) => {
                 w="100%"
             >
                 <Group w="100%" style={{borderRadius:"50px", height:"40px"}} ff={"Oswald"}>
-                    <Text ta="center" c="#8E8D8A" pl="10px" size="lg" w="20%">Aktualny Cel</Text>
-                    <Group c="white" bg="#E98074" miw="40%" maw="60%" style={{borderRadius:"200px"}} ff={"Oswald"} justify="center">
-                        <Text ta="center" pl="10px" size="lg" w="60%">{activeObjective[0].title}</Text>
-                        <Group bg="#8E8D8A" miw="20%" maw="50%" style={{borderRadius:"200px"}} ff={"Oswald"} justify="center">
-                            <Text>{activeObjective[0].current_points}/{activeObjective[0].max_points}</Text>
+                    <Text ta="center" c="#8E8D8A" pl="10px" size="30px" w="20%">Aktualny Cel</Text>
+                    <Group c="white" bg="#E98074" miw="40%" maw="60%" h="40px" style={{borderRadius:"200px"}} ff={"Oswald"} justify="center">
+                        <Text ta="center" pl="10px" size="20px" w="60%">{activeObjective[0].title}</Text>
+                        <Group bg="#8E8D8A" miw="20%" maw="50%" h="30px" style={{borderRadius:"200px"}} ff={"Oswald"} justify="center">
+                            <Text size='20px'>{activeObjective[0].current_points}/{activeObjective[0].max_points}</Text>
                         </Group>
                     </Group>
                     <EditActiveObjectiveModal userEmail={email} getData={getData}/>
--- src/components/modals/CreateObjectiveModal.jsx
+++ src/components/modals/CreateObjectiveModal.jsx
@@ -1,5 +1,5 @@
 import { useDisclosure } from '@mantine/hooks';
-import { Modal, TextInput, NumberInput, Button} from '@mantine/core';
+import { Text, Modal, TextInput, NumberInput, Button} from '@mantine/core';
 import { useForm } from '@mantine/form';
 
 const CreateObjectiveModal = ({email, getData}) => {
@@ -35,33 +35,33 @@ const CreateObjectiveModal = ({email, getData}) => {
         <>
             <Modal opened={opened} onClose={close} title="Dodaj nowy cel" centered>
                 <form onSubmit={form.onSubmit((values) => createObjective({...values, email}))}>
+                    <Text size="20px">Tytuł</Text>
                     <TextInput
                         withAsterisk
-                        label="Tytuł"
                         {...form.getInputProps('title')}
                     />
+                    <Text size="20px">Minimalna ilość punktów</Text>
                     <NumberInput
                         withAsterisk
-                        label="Minimalna ilość punktów"
                         min={0}
                         max={99}
                         {...form.getInputProps('min_points')}
                     />
+                    <Text size="20px">Maksymalna ilość punktów</Text>
                     <NumberInput
                         withAsterisk
-                        label="Maksymalna ilość punktów"
                         min={1}
                         max={99}
                         {...form.getInputProps('max_points')}
                     />
-                    <Button onClick={close} type="submit">Wyślij</Button>
+                    <Button bg="#E98074" size='md' onClick={close} type="submit">Wyślij</Button>
                 </form>
             </Modal>
 
             <Button onClick={() => {
                 open(),
                 form.reset()
-            }} bg="#E98074" style={{borderRadius:"50px", fontSize:"15px", fontWeight:"normal"}} ff={"Oswald"}>Dodaj nowy cel</Button>
+            }} bg="#E98074" style={{borderRadius:"50px", fontSize:"25px", fontWeight:"normal"}} ff={"Oswald"}>Dodaj nowy cel</Button>
         </>
     )
 }
